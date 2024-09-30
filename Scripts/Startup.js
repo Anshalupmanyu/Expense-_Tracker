3e83ebe47640e99ede17af3a6d7c8861f2e3334b
DD34THH <anshal.u.2002@gmail.com>
2024-09-30 22:48:10 +0530

    First Commit

---
+++ Scripts/Startup.js
@@ -0,0 +1,15 @@
+const getStartedBtn = document.getElementById('login');
+const dialog = document.querySelector('.auth-container');
+const closeBtn = document.getElementById('dialog');
+
+getStartedBtn.addEventListener('click', () => {
+  dialog.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.style.display = 'none';
+});
+
+document.getElementById("signup").addEventListener('click', function() {
+    window.location.href = '../Pages/Homepage.html';
+})
